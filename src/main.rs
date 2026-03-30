97f955f3966d3690ad5bba5016b689b41994f3a7
Blair Xu <masterystacker@gmail.com>
2026-03-30 13:43:38 -0400

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,30 @@
 #[allow(unused_imports)]
 use std::net::TcpListener;
-use std::io::Write;
+use std::{
+    io::{Read, Write},
+    net::TcpStream,
+};
+
+
+fn handle_connection(mut stream: TcpStream){
+    let mut buffer = [0;1024];  
+    let bytes_read =stream.read(&mut buffer).unwrap();
+
+    let request_data = &buffer[..bytes_read];
+
+    let parts: Vec<&[u8]> = request_data.split(|&c| c==b' ').collect();
+
+    let response = match parts[1]{
+        b"/" => "HTTP/1.1 200 OK \r\n\r\n",
+        _ =>"HTTP/1.1 404 NOT FOUND \r\n\r\n",
+    };
+
+    stream.write_all(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+
+}
+
+
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -10,9 +34,8 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(mut _stream) => {
-                println!("connection established");
-                _stream.write_all(b"HTTP/1.1 200 OK\r\n\r\n").unwrap();
+            Ok(stream) => {
+               handle_connection(stream)
             }
             Err(e) => {
                 println!("error: {}", e);
@@ -20,3 +43,4 @@ fn main() {
         }
     }
 }
+
