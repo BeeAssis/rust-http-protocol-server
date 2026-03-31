d6cc3c28f9d96088221856e48cdaa35d3f03e579
Blair Xu <masterystacker@gmail.com>
2026-03-31 15:23:45 -0400

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -5,29 +5,35 @@ use std::{
     net::TcpStream,
 };
 
-
-fn handle_connection(mut stream: TcpStream){
-    let mut buffer = [0;1024];  
-    let bytes_read =stream.read(&mut buffer).unwrap();
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    let bytes_read = stream.read(&mut buffer).unwrap();
 
     let request_data = &buffer[..bytes_read];
+    let parts: Vec<&[u8]> = request_data.split(|&c| c == b' ').collect();
 
-    let parts: Vec<&[u8]> = request_data.split(|&c| c==b' ').collect();
+    let path = parts[1];
 
-    let response = match parts[1]{
-        b"/" => "HTTP/1.1 200 OK\r\n\r\n",
-        _ =>"HTTP/1.1 404 Not Found\r\n\r\n",
+    let response = if path == b"/" {
+        "HTTP/1.1 200 OK\r\n\r\n".to_string()
+    } else if path.starts_with(b"/echo/") {
+        let body = &path[6..];
+        let content_length = body.len();
+
+        format!(
+            "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
+            content_length,
+            std::str::from_utf8(body).unwrap(),
+        )
+    } else {
+        "HTTP/1.1 404 Not Found\r\n\r\n".to_string()
     };
 
     stream.write_all(response.as_bytes()).unwrap();
     stream.flush().unwrap();
-
 }
 
-
-
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
@@ -35,7 +41,7 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
-               handle_connection(stream)
+                handle_connection(stream)
             }
             Err(e) => {
                 println!("error: {}", e);
@@ -43,4 +49,3 @@ fn main() {
         }
     }
 }
-
